e4301bbd10aaab95e200c600c8f1a9e4e0e64841
Pedro Duarte Bairao <pedro.bairao@gmail.com>
2018-01-10 21:35:28 +0100

    feat(switch): able to switch light and ringer from the app and handles MQTT

--- app/src/main/java/com/example/pedrp/roommanagement/ContextManagementActivity.java
+++ app/src/main/java/com/example/pedrp/roommanagement/ContextManagementActivity.java
@@ -3,29 +3,61 @@ package com.example.pedrp.roommanagement;
 import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.TextView;
+
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttCallbackExtended;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.MqttPersistenceException;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import helpers.MqttHelper;
+import helpers.MqttSubscriber;
 
 public class ContextManagementActivity extends AppCompatActivity {
 
-    private String room = "";
+    private String room = null;
     private RoomContextState state;
+    MqttHelper mqttHelper;
+    MqttSubscriber mqttSubscriber;
+    /*
+    MemoryPersistence memPer = new MemoryPersistence();
 
+    final MqttAndroidClient client = new MqttAndroidClient(
+            this, "wss://m23.cloudmqtt.com:34160", "roomPublisher", memPer);
+    */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        System.out.println("created!");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_context_management);
+
+        //initialize the MQTT Client
+        startMqtt();
+        startSubscriber();
+
+        //Retrieve the Rooms
+        retrieveRooms();
+
         //Retrieve Room
         ((Button) findViewById(R.id.buttonCheck)).setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                System.out.println("Click");
-                room = ((EditText) findViewById(R.id.editText1))
-                        .getText().toString();
-                retrieveRoomContextState(room);
+                room = ((Spinner) findViewById(R.id.spinner)).getSelectedItem().toString();
+                if (room != null)
+                    retrieveRoomContextState(room);
             }
         });
 
@@ -33,7 +65,11 @@ public class ContextManagementActivity extends AppCompatActivity {
         ((Button) findViewById(R.id.button1)).setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 System.out.println("SwitchLight");
-                switchLight(room);
+                if (room != null) {
+                    switchLight(room);
+                    publish(room, "light");
+                }
+
             }
         });
 
@@ -41,9 +77,13 @@ public class ContextManagementActivity extends AppCompatActivity {
         ((Button) findViewById(R.id.button2)).setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 System.out.println("SwitchRinger");
-                switchRinger(room);
+                if (room != null) {
+                    switchRinger(room);
+                    publish(room, "ringer");
+                }
             }
         });
+
     }
 
     protected void onUpdate(RoomContextState context){
@@ -87,4 +127,105 @@ public class ContextManagementActivity extends AppCompatActivity {
         RoomContextHttpManager.switchRinger(room, this, this.getApplicationContext());
     }
 
+    protected void retrieveRooms(){
+        System.out.println("Retrieve Rooms");
+        RoomContextHttpManager.retrieveRooms(this, this.getApplicationContext());
+    }
+
+    protected void fillSpinner(List<String> list){
+
+        //Spinner
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+
+        // Creating adapter for spinner
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, list);
+
+        // Drop down layout style - list view with radio button
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // attaching data adapter to spinner
+        spinner.setAdapter(dataAdapter);
+    }
+
+    private void startMqtt(){
+        mqttHelper = new MqttHelper(getApplicationContext());
+        mqttHelper.setCallback(new MqttCallbackExtended() {
+            @Override
+            public void connectComplete(boolean b, String s) {
+                System.out.println("Connect Complete");
+
+            }
+
+            @Override
+            public void connectionLost(Throwable throwable) {
+                System.out.println("Connect Lost");
+            }
+
+            @Override
+            public void messageArrived(String topic, MqttMessage mqttMessage) throws Exception {
+                Log.w("Debug",mqttMessage.toString());
+                System.out.println(mqttMessage.toString());
+                System.out.println("Message Received");
+            }
+
+            @Override
+            public void deliveryComplete(IMqttDeliveryToken iMqttDeliveryToken) {
+
+            }
+        });
+    }
+
+    private void startSubscriber(){
+        mqttSubscriber = new MqttSubscriber(getApplicationContext());
+        mqttSubscriber.setCallback(new MqttCallbackExtended() {
+            @Override
+            public void connectComplete(boolean b, String s) {
+                System.out.println("Connect Complete");
+
+            }
+
+            @Override
+            public void connectionLost(Throwable throwable) {
+                System.out.println("Connect Lost");
+            }
+
+            @Override
+            public void messageArrived(String topic, MqttMessage mqttMessage) throws Exception {
+                Log.w("Debug",mqttMessage.toString());
+                System.out.println(mqttMessage.toString());
+                updateMqtt(topic, mqttMessage.toString());
+            }
+
+            @Override
+            public void deliveryComplete(IMqttDeliveryToken iMqttDeliveryToken) {
+
+            }
+        });
+    }
+
+    private void publish(String room, String lightnoise) {
+        MqttMessage message = new MqttMessage("switch,appClient".getBytes());
+        try {
+            mqttHelper.mqttAndroidClient.publish("rooms/" + room + "/" + lightnoise, message);
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void updateMqtt(String topic, String message){
+        System.out.println("Update MQTT");
+        if (!message.toString().contains(",")) {
+            System.out.println("Bad message received.");
+            return;
+        }
+        String sender = message.toString().split(",")[1];
+        message = message.toString().split(",")[0];
+        System.out.println("[mqtt] " + topic + " : " + message + " from " + sender);
+        //String type = topic.split("/")[2];
+        String roomId = topic.split("/")[1];
+        System.out.println(roomId);
+        if (message.equals("switch") && (sender.equals("arduinoClient") || sender.equals("webClient")) && room.equals(roomId)) {
+            retrieveRoomContextState(roomId);
+        }
+    }
 }
